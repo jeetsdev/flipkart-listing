91f3e6b9f4ef35cee061a2c9fc5b08865dff59b0
Ranjeet Singh <44586579+jeetsdev@users.noreply.github.com>
2022-08-27 16:47:03 +0530

    feat : no products error

--- src/Pages/ProductListing.jsx
+++ src/Pages/ProductListing.jsx
@@ -1,4 +1,3 @@
-import { useEffect } from "react";
 import { Filter, Header, ProductCard } from "../components";
 import { useFilter } from "../contexts/filter-context";
 import productData from "../data/data.json";
@@ -8,7 +7,8 @@ import "./ProductListing.css";
 export const ProductListing = () => {
 	const { filterData } = useFilter();
 	const finalData = filterProduct(filterData, productData);
-	
+	console.log("finalData: ", finalData.lenght);
+
 	return (
 		<div className="">
 			<nav>
@@ -16,12 +16,23 @@ export const ProductListing = () => {
 			</nav>
 			<div className="product-container ">
 				<Filter />
-				<div className="flex flex-wrap gap-2">
-					{finalData?.map((product) => {
-						return (
-							<ProductCard product={product} key={product.id} />
-						);
-					})}
+				<div>
+					{finalData.length === 0 ? (
+						<p className="text-center text-3xl">
+							No products found, please clear filters.
+						</p>
+					) : (
+						<div className="flex flex-wrap gap-2">
+							{finalData?.map((product) => {
+								return (
+									<ProductCard
+										product={product}
+										key={product.id}
+									/>
+								);
+							})}
+						</div>
+					)}
 				</div>
 			</div>
 		</div>
